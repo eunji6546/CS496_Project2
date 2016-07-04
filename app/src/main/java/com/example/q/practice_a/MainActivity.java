0a8cc6027c9180ad4833b78aea9314f64f7cb43c
JuHee <dlwngml0504@naver.com>
2016-07-04 19:53:17 +0900

    implement http connection

--- app/src/main/java/com/example/q/practice_a/MainActivity.java
+++ app/src/main/java/com/example/q/practice_a/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.q.practice_a;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -20,6 +27,33 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends FragmentActivity {
@@ -41,34 +75,26 @@ public class MainActivity extends FragmentActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Toast.makeText(getApplicationContext(), "Login Succeed", Toast.LENGTH_SHORT).show();
-                final Intent intent = new Intent(MainActivity.this,MyActivity.class);
-                new GraphRequest(
-                        AccessToken.getCurrentAccessToken(),
-                        "/{education-experience-id}",
-                        null,
-                        HttpMethod.GET,
-                        new GraphRequest.Callback() {
-                            public void onCompleted(GraphResponse response) {
-            /* handle the result */intent.putExtra("friends",response.toString());
-                            }
-                        }
-                ).executeAsync();
+
                 new GraphRequest(
                         AccessToken.getCurrentAccessToken(),
                         "/me/taggable_friends",
                         null,
                         HttpMethod.GET,
                         new GraphRequest.Callback() {
-                            public void onCompleted(GraphResponse response) {
-
+                            public void onCompleted(final GraphResponse response) {
+                                new Thread(){
+                                    public void run() {
+                                        new HttpConnectionThread().doInBackground("http://143.248.47.163:3000/insert",response.toString());
+                                    }
+                                }.start();
+                                Intent intent = new Intent(MainActivity.this,MyActivity.class);
                                 intent.putExtra("taggable_friends",response.toString());
                                 startActivity(intent);
 
                             }
                         }
                 ).executeAsync();
-
-
             }
 
             @Override
@@ -83,31 +109,49 @@ public class MainActivity extends FragmentActivity {
         });
     }
 
+    public class HttpConnectionThread extends AsyncTask<String,Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+            URL murl;
+            String response = null;
+
+            try {
+                murl = new URL(url[0]);
+                HttpURLConnection conn = (HttpURLConnection) murl.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.connect();
+                conn.getOutputStream();
+                OutputStream os =  conn.getOutputStream();
+                os.write(url[1].getBytes("UTF-8"));
+                os.flush();
+                os.close();
+                response = conn.getResponseMessage();
+
+            } catch (IOException e) {
+
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-        //Toast.makeText(getApplicationContext(), "onActiveResult~!!", Toast.LENGTH_SHORT).show();
     }
 
-
-        /*try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                "com.example.q.practice_a",
-                PackageManager.GET_SIGNATURES);
-            for(android.content.pm.Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:",Base64.encodeToString(md.digest(), Base64.DEFAULT));
-
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        }catch (NoSuchAlgorithmException e) {
-
-        }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -127,7 +171,6 @@ public class MainActivity extends FragmentActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
